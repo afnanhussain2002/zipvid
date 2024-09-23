8674490b6750a667994161fddb95cb18ca9c27fd
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-23 20:54:57 +0600

    write a get func and connect prisma

--- src/app/api/video/route.ts
+++ src/app/api/video/route.ts
@@ -2,3 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+export async function GET(req: NextRequest) {
+    await prisma.$connect();
+}

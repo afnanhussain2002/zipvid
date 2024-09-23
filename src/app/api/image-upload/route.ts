b509ba1a5cc8554935f93964092b7738d8e22600
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-23 22:27:34 +0600

    write promise for upload image

--- src/app/api/image-upload/route.ts
+++ src/app/api/image-upload/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse, NextRequest } from "next/server";
 import { auth } from "@clerk/nextjs/server";
 import { v2 as cloudinary } from 'cloudinary';
+import { resolve } from "path";
+import { error } from "console";
 
 
 
@@ -13,7 +15,7 @@ import { v2 as cloudinary } from 'cloudinary';
 
 interface CoudinaryUploadResult {
     public_id: string;
-   [key: string]: any;
+   [key: string]: string;
 }
 
 
@@ -33,6 +35,18 @@ export async function POST(request:NextRequest){
 
        const bytes = await file.arrayBuffer()
        const buffer = Buffer.from(bytes)
+
+       await new Promise<CoudinaryUploadResult>(
+        (resolve,reject) =>{
+            cloudinary.uploader.upload_stream(
+                {folder:"zipvid-image-uploads"},
+                (error,result) =>{
+                    if (error) reject(error);
+                    else resolve(result as CoudinaryUploadResult)
+                }
+            )
+        }
+       )
     } catch (error) {
         
     }

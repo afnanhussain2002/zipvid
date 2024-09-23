91ba72a559e8fe84ec5f262d306eeb1588815295
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-23 22:17:20 +0600

    try to get a file

--- src/app/api/image-upload/route.ts
+++ src/app/api/image-upload/route.ts
@@ -3,6 +3,7 @@ import { auth } from "@clerk/nextjs/server";
 import { v2 as cloudinary } from 'cloudinary';
 
 
+
   // Configuration
   cloudinary.config({ 
     cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME, 
@@ -17,9 +18,19 @@ interface CoudinaryUploadResult {
 
 
 export async function POST(request:NextRequest){
-    const {userId} = auth()
+    try {
+        const {userId} = auth()
+    
+       if (!userId) {
+        return NextResponse.json({error: "Unauthorized"}, {status:401})
+       }
+       const formData = await request.formData();
+       const file = formData.get("file") as File | null
 
-   if (!userId) {
-    return NextResponse.json({error: "Unauthorized"}, {status:401})
-   }
+       if (!file) {
+        return NextResponse.json({error:"File not found"}, {status:400})
+       }
+    } catch (error) {
+        
+    }
 }

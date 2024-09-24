b4b1bb6cfaa75c0eefa6b10b3b35da72fbeac2d7
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-24 07:45:06 +0600

    add some social formats

--- src/app/(app)/social-formats/page.tsx
+++ src/app/(app)/social-formats/page.tsx
@@ -1,10 +1,18 @@
 
-import React, { use, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 
+const socialFormats = {
+    "Instagram Square (1:1)": { width: 1080, height: 1080, aspectRatio: "1:1" },
+    "Instagram Portrait (4:5)": { width: 1080, height: 1350, aspectRatio: "4:5" },
+    "Twitter Post (16:9)": { width: 1200, height: 675, aspectRatio: "16:9" },
+    "Twitter Header (3:1)": { width: 1500, height: 500, aspectRatio: "3:1" },
+    "Facebook Cover (205:78)": { width: 820, height: 312, aspectRatio: "205:78" },
+  };
+  type SocialFormat = keyof typeof socialFormats;
 function SocialFormats() {
 
     const [uploadImage, setUploadImage] = useState<string | null>(null);
-    const [selectedFormat, setSelectedFormat] = useState<SocialFormats>(null);
+    const [selectedFormat, setSelectedFormat] = useState<SocialFormat>("Instagram Square (1:1)");
     const [isUploading, setIsUploading] = useState(false);
     const [isTransforming, setIsTransforming] = useState(false);
     const imageRef = useRef<HTMLImageElement>(null);

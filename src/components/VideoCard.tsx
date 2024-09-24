5d4da8afc7684d0e822a9323135ee15843f8e12a
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-24 16:01:34 +0600

    define getCldImageUrl

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -5,6 +5,7 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import {filesize} from 'filesize';
 import { Video } from '@prisma/client';
+import { get } from 'http';
 
 dayjs.extend(relativeTime);
 
@@ -18,6 +19,16 @@ const VideoCard: React.FC<VideoCardProps> = ({video, onDownload}) => {
     const [previewError, setPreviewError] = useState(false);
 
     const getThumbnailUrl = useCallback((publicId: string) => {
+
+        return getCldImageUrl({
+            src: publicId,
+            width: 400,
+            height: 225,
+            crop: "fill",
+            quality: "auto",
+            format: "jpg",
+            assetType: "video",
+        })
         
     },[])
     return (

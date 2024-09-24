d2bbbb1061aa4f001c7d81506ca901c53bf4778b
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-24 15:47:03 +0600

    import some needed packages

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,4 +1,8 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect, useCallback} from 'react';
+import { getCldImageUrl, getCldVideoUrl } from 'next-cloudinary';
+import { Download, Clock, FileDown, FileUp } from 'lucide-react';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 
 const VideoCard = () => {

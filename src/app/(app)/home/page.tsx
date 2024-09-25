fad1fe85483d99d4897d9dd78a787b4fb393bae0
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-25 11:59:35 +0600

    define some states

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -3,7 +3,11 @@ import React,{ useState , useEffect,useCallback} from "react";
 import axios from "axios";
 import VideoCard from "@/components/VideoCard";
 
+
 function Home() {
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   return <div>Home</div>;
 }
 

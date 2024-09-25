e3eebce46812fac6f3e99a11785a98b4fd9787db
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-25 11:56:47 +0600

    import video card

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React,{ useState , useEffect,useCallback} from "react";
 import axios from "axios";
+import VideoCard from "@/components/VideoCard";
 
 function Home() {
   return <div>Home</div>;

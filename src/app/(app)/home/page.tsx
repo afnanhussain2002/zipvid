64c07b041f6a86cebcd112fc2de667a1eb18d364
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-09-25 12:19:48 +0600

    work with the handle download part

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -1,14 +1,12 @@
 "use client";
-import React,{ useState , useEffect,useCallback} from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import VideoCard from "@/components/VideoCard";
-
 
 function Home() {
   const [videos, setVideos] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-
 
   const fetchVideos = useCallback(async () => {
     try {
@@ -17,14 +15,27 @@ function Home() {
     } catch (error) {
       console.log(error);
       setError("Error fetching videos");
-    }finally{
+    } finally {
       setLoading(false);
     }
-  },[])
+  }, []);
 
   useEffect(() => {
     fetchVideos();
   }, [fetchVideos]);
+
+  const handleDownload = useCallback((url: string, title: string) => {
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", `${title}.mp4`);  // Set the desired filename here
+    link.setAttribute("target", "_blank");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    window.URL.revokeObjectURL(url);
+  }, []);
+
+
 
   return <div>Home</div>;
 }
